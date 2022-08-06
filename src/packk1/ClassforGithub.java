0cc976463b697dc313152147f72e30225b484799
priya <priya@LAPTOP-80CMDR21>
2022-08-06 23:51:54 +0530

    commit3

--- src/packk1/ClassforGithub.java
+++ src/packk1/ClassforGithub.java
@@ -7,6 +7,8 @@ public static void main(String[] args)
 	System.out.println("I am a Software Test Engineer");
 	System.out.println ("i have around 2.8 years of experiance");
 	System.out.println ("I am responsible for creation of test script");
+	System.out.println ("hii hello by");
+
 
 	
 }
